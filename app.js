26680e88bd247cc87b02f049a2aed2881160f775
mkmmb <108124387+mkmmb@users.noreply.github.com>
2022-06-24 19:45:33 +0800

    Create app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+{
+  "pages":[
+    "index/index",
+    "page2/page2"
+    ],
+    "window":{
+      "backgroundTextStyle": "light",
+      "navigationBarBackgroundColor": "#fff",
+      "navigationBarTitleText": "智能光环境控制",
+      "navigationBarTextStyle": "black"
+    },
+  
+  "sitemapLocation": "sitemap.json"
+}
